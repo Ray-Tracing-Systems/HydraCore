9b0b8607c34a6eb6066b770d2ae5e3d8a578885e
vsan-msu <vsan@protonmail.com>
2018-12-03 18:13:00 +0300

    inDevelopment=false

--- hydra_app/input.cpp
+++ hydra_app/input.cpp
@@ -33,7 +33,7 @@ Input::Input()
   //inLibraryPath   = "/media/frol/6E0467C0046789C3/[Hydra]/pluginFiles/scenelib";
   //inLibraryPath = "D:/temp/scenelib/"; 
 
-  inDevelopment = true;  ///< recompile shaders each time; note that nvidia have their own shader cache!
+  inDevelopment = false;  ///< recompile shaders each time; note that nvidia have their own shader cache!
   inDeviceId    = 0;     ///< opencl device id
   cpuFB         = true;  ///< store frame buffer on CPU. Automaticly enabled if
   enableMLT     = false; ///< if use MMLT, you MUST enable it early, when render process just started (here or via command line).
