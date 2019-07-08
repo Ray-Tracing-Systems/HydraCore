277eb64dcfafa34a867e5a7ed291e9ae30a32057
vsan-msu <vsan@protonmail.com>
2019-07-08 14:56:01 +0300

    remove old debug stuff (extern g_drawBVHIdInput and another copy of s2ws)

--- hydra_app/main_app_window.cpp
+++ hydra_app/main_app_window.cpp
@@ -20,7 +20,6 @@ int         g_width  = 1024;
 int         g_height = 1024;
 
 extern Input g_input;
-extern int   g_drawBVHIdInput;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 static bool   g_captureMouse         = false;
@@ -34,21 +33,6 @@ static HRCameraRef    camRef;
 static HRSceneInstRef scnRef;
 static HRRenderRef    renderRef;
 
-static std::wstring s2ws(const std::string& s)
-{
-  #ifdef WIN32
-  int len;
-  int slength = (int)s.length() + 1;
-  len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
-  wchar_t* buf = new wchar_t[len];
-  MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
-  std::wstring r(buf);
-  delete[] buf;
-  return r;
-  #else
-  return std::wstring(s.begin(), s.end());
-  #endif
-}
 
 static void Init(std::shared_ptr<IHRRenderDriver> pDriverImpl)
 {
@@ -345,16 +329,6 @@ static void key(GLFWwindow* window, int k, int s, int action, int mods)
 
   case GLFW_KEY_LEFT_SHIFT:
     g_input.camMoveSpeed = 10.0f;
-    break;
-
-  case GLFW_KEY_PAGE_UP:
-    g_drawBVHIdInput--;
-    if (g_drawBVHIdInput < 0)
-      g_drawBVHIdInput = 0;
-    break;
-
-  case GLFW_KEY_PAGE_DOWN:
-    g_drawBVHIdInput++;
     break;
 
   case GLFW_KEY_P:
