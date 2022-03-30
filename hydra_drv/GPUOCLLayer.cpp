78dac72338cd26b16b106a5d14e5c7e572e03220
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-03-30 12:45:40 +0300

    edit

--- hydra_drv/GPUOCLLayer.cpp
+++ hydra_drv/GPUOCLLayer.cpp
@@ -1164,8 +1164,8 @@ void GPUOCLLayer::InitPathTracing(int seed)
   
   // reset camera plugin if we have it ...
   //
-  
-  if(m_camPlugin.pCamPlugin == nullptr && m_camNode.attribute(L"cpu_plugin").as_int() != 0) // actual plugin init happened first time
+  const int cpuPluginId = m_camNode.attribute(L"cpu_plugin").as_int();
+  if(m_camPlugin.pCamPlugin == nullptr && cpuPluginId != 0) // actual plugin init happened first time
   {
     const wchar_t* dllPath = m_camNode.attribute(L"cpu_plugin_dll").as_string();
     if(std::wstring(dllPath) != L"")
