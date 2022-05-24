73273c836a2cde12d870e76d656fc5e334049dc9
Vadim Sanzharov <vadim.sanzharov@graphics.cs.msu.ru>
2022-05-24 13:34:15 +0300

    winapi loadlibrary error

--- cam_plug/HydraDLibWin64.cpp
+++ cam_plug/HydraDLibWin64.cpp
@@ -1,6 +1,8 @@
 #include "HydraDLib.h"
 #include <string>
 #include <windows.h>
+#include <iostream>
+#include <strsafe.h>
 
 std::string  ws2s(const std::wstring& s);
 
@@ -10,6 +12,31 @@ HydraDllHandle g_handle;
 HydraDllHandle* HydraLoadLibrary(const wchar_t* a_path)
 {
   g_handle.handle = LoadLibraryW(a_path);
+  if (g_handle.handle == nullptr)
+  {
+    DWORD dw = GetLastError();
+    LPWSTR lpMsgBuf;
+    LPVOID lpDisplayBuf;
+  
+    FormatMessage(
+      FORMAT_MESSAGE_ALLOCATE_BUFFER |
+      FORMAT_MESSAGE_FROM_SYSTEM |
+      FORMAT_MESSAGE_IGNORE_INSERTS,
+      NULL,
+      dw,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      (LPTSTR)&lpMsgBuf,
+      0, NULL);
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, (lstrlen((LPCTSTR)lpMsgBuf) + 512) * sizeof(TCHAR));
+
+    StringCchPrintf((LPTSTR)lpDisplayBuf, LocalSize(lpDisplayBuf) / sizeof(TCHAR), TEXT("Hydra failed loading DLL from %s with error %d: %s"), a_path, dw, lpMsgBuf);
+
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+  }
   return &g_handle;
 }
 
